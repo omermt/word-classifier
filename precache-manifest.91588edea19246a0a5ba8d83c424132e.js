9fc1158846d707338ac3737b6e5f234dcaddbee4
Omer Marquez <omer.marquez54@gmail.com>
2020-06-16 13:45:13 -0400

    Updates

---
+++ precache-manifest.91588edea19246a0a5ba8d83c424132e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b147f9a1fb9787196df5cbb65a052941",
+    "url": "/word-classifier/index.html"
+  },
+  {
+    "revision": "fb0304228582c866de8a",
+    "url": "/word-classifier/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "77bbe0c7e639a44e5a3c",
+    "url": "/word-classifier/static/js/2.7c103051.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/word-classifier/static/js/2.7c103051.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb0304228582c866de8a",
+    "url": "/word-classifier/static/js/main.4db8120b.chunk.js"
+  },
+  {
+    "revision": "2c615e8d1f3f267689a4",
+    "url": "/word-classifier/static/js/runtime-main.c1ceb3f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/word-classifier/static/media/logo.5d5d9eef.svg"
+  }
+]);
