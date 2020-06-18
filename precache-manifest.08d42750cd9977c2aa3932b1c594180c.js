e8e3f82f65811d1342c8b96cb8b6417a83a7ced6
Omer Marquez <omer.marquez54@gmail.com>
2020-06-18 13:53:12 -0400

    Updates

---
+++ precache-manifest.08d42750cd9977c2aa3932b1c594180c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ce5cb2ef38e3ddd58aa6e3cee66c20",
+    "url": "/word-classifier/index.html"
+  },
+  {
+    "revision": "89190e703c57872153f6",
+    "url": "/word-classifier/static/css/2.cc1a181b.chunk.css"
+  },
+  {
+    "revision": "5047db49306e0e8c2b53",
+    "url": "/word-classifier/static/css/main.f50d1e9d.chunk.css"
+  },
+  {
+    "revision": "89190e703c57872153f6",
+    "url": "/word-classifier/static/js/2.8bc9804d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/word-classifier/static/js/2.8bc9804d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5047db49306e0e8c2b53",
+    "url": "/word-classifier/static/js/main.30819d70.chunk.js"
+  },
+  {
+    "revision": "2c615e8d1f3f267689a4",
+    "url": "/word-classifier/static/js/runtime-main.c1ceb3f1.js"
+  },
+  {
+    "revision": "08a4cb53f49475e4f63f74a0d0a0d1a5",
+    "url": "/word-classifier/static/media/Asai-Analogue.08a4cb53.ttf"
+  },
+  {
+    "revision": "5f7469dcc2b98725cb6aaf42009d4d9b",
+    "url": "/word-classifier/static/media/pepe2.5f7469dc.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/word-classifier/precache-manifest.4a94053b02cfbe0859d7658cf6a5d797.js"
+  "/word-classifier/precache-manifest.08d42750cd9977c2aa3932b1c594180c.js"
 );
 
 self.addEventListener('message', (event) => {
